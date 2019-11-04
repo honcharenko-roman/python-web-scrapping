47675598e81fd6e83a10e0ea0d1de69aff07d622
yunhaveto <yunhaveto@gmail.com>
2019-11-04 23:30:49 +0200

    added async kreygams

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import requests
 import time
 import selenium
+import asyncio
+import aiohttp
+import html5lib
 
 from bs4 import BeautifulSoup
 from datetime import date
@@ -8,7 +11,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
-# TODO Async scrapping 
+# TODO Async scrapping
 # TODO City name parcing (work.ua , ...)
 
 
@@ -25,7 +28,7 @@ def dou_jobs(keyword, city):
     works_dict = {
 
     }
-    
+
     driver = set_up_chrome_driver()
     url = 'https://jobs.dou.ua/vacancies/?city=' + city + '&category=' + keyword
     driver.get(url)
@@ -54,7 +57,7 @@ def work_ua(keyword, city):
     url = 'https://www.work.ua/jobs-' + city + '-' + keyword + '/?page='
 
     while True:
-        soup = soup_site(url, pageid=pageid)
+        soup = sync_site_soup(url, pageid=pageid)
         headline_tags = soup.findAll('h2', {'class': 'add-bottom-sm'})
         if not headline_tags:
             break
@@ -73,7 +76,7 @@ def djinni(keyword, city):
     url = 'https://djinni.co/jobs/?primary_keyword=' + keyword + '&page='
     closer = '&location=' + city
     while True:
-        soup = soup_site(url, pageid=pageid, closer=closer)
+        soup = sync_site_soup(url, pageid=pageid, closer=closer)
         headline_tags = soup.findAll(
             'a', {'class': 'profile'})
         if headline_tags:
@@ -99,7 +102,7 @@ def rabota_ua(keyword, city=None):
         site_url = 'https://rabota.ua/zapros/' + keyword + '/pg'
 
     while True:
-        soup = soup_site(site_url, pageid)
+        soup = sync_site_soup(site_url, pageid)
         headline_tags = soup.findAll(
             'a', {'class': 'f-visited-enable ga_listing'})
         if headline_tags:
@@ -112,7 +115,7 @@ def rabota_ua(keyword, city=None):
     return works_dict
 
 
-def soup_site(address, pageid=None, closer=None):
+def sync_site_soup(address, pageid=None, closer=None):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.78 Safari/537.36 Vivaldi/2.8.1664.35'
     }
@@ -124,6 +127,23 @@ def soup_site(address, pageid=None, closer=None):
         url = address
     response = requests.get(url, headers=headers)
     return BeautifulSoup(response.text, 'html.parser')
+
+
+async def async_site_soup(address, pageid=None, closer=None):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.78 Safari/537.36 Vivaldi/2.8.1664.35'
+    }
+    if pageid and closer:
+        url = '%s%s%s' % (address, pageid, closer)
+    elif pageid:
+        url = '%s%s' % (address, pageid)
+    else:
+        url = address
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, headers=headers) as resp:
+            text = await resp.read()
+
+    return BeautifulSoup(text.decode('utf-8'), 'html5lib')
 
 
 def dota_news(find_value):
@@ -149,7 +169,7 @@ def dota_news(find_value):
     }
 
     while True:
-        soup = soup_site('https://dota2.net/allnews?page=', pageid=pageid)
+        soup = sync_site_soup('https://dota2.net/allnews?page=', pageid=pageid)
         time_tags = soup.select("div[class=news-item__statistics]")
 
         for time_tag in time_tags:
@@ -173,49 +193,55 @@ def dota_news(find_value):
     return news_dict
 
 
-def dota_radiant_winrate():
-    radiant_winners = 0
-    overall_games_count = 0
-    pageid = 1
+async def dota_winners_coroutine(radiant_winners, overall_games, pageid):
+    soup = await async_site_soup('https://www.dotabuff.com/esports/matches?page=',
+                            pageid=pageid)
+    headline_tags = soup.findAll(
+        'span', {'class': 'team-text team-text-full'})
 
-    while True:
-        print(pageid)
-        soup = soup_site('https://www.dotabuff.com/esports/matches?page=',
-                         pageid=pageid)
-        headline_tags = soup.findAll(
-            'span', {'class': 'team-text team-text-full'})
+    if not headline_tags:
+        return
 
-        if not headline_tags:
-            break
-
-        for headline_tag in headline_tags:
-            if str(headline_tag.parent.parent['class']).find('radiant') > 0 \
-                    and str(headline_tag.parent.parent.parent['class']).find('winner') > 0:
-                radiant_winners += 1
-        overall_games_count += 20
-
-        pageid += 1
-
-    if overall_games_count != 0:
-        return radiant_winners / overall_games_count
-    else:
-        return 0
+    for headline_tag in headline_tags:
+        if str(headline_tag.parent.parent['class']).find('radiant') > 0 \
+                and str(headline_tag.parent.parent.parent['class']).find('winner') > 0:
+            radiant_winners.append(1)
+    overall_games.append(20)
 
 
-def habr_python_articles(find_value):
-    pageid = 1
+#TODO STOP RANGE mb flag!
+async def dota_radiant_winrate():
+    radiant_winners = []
+    overall_games = []
+
+    tasks = [
+        asyncio.create_task(dota_winners_coroutine(radiant_winners, overall_games, i)) for i in range(1, 50)
+    ]
+    await asyncio.wait(tasks)
+
+    return sum(radiant_winners) / sum(overall_games)
+
+
+async def habr_coroutine(headline_link_dict, pageid, find_value):
+    soup = await async_site_soup('https://habr.com/en/all/page', pageid=pageid)
+    for headline_tag in soup.findAll('a', {'class': 'post__title_link'}):
+        result = str(headline_tag.contents).lower().find(
+            find_value.lower())
+        if result > 0:
+            headline_link_dict[str(headline_tag.contents[0])
+                               ] = headline_tag['href']
+        else:
+            continue
+
+
+# TODO range
+async def habr_python_articles(find_value):
     headline_link_dict = {
     }
-    for pageid in range(1, 10):
-        soup = soup_site('https://habr.com/en/all/page', pageid=pageid)
-        for headline_tag in soup.findAll('a', {'class': 'post__title_link'}):
-            result = str(headline_tag.contents).lower().find(
-                find_value.lower())
-            if result > 0:
-                headline_link_dict[str(headline_tag.contents[0])
-                                   ] = headline_tag['href']
-            else:
-                continue
+    tasks = [
+        asyncio.create_task(habr_coroutine(headline_link_dict, i, find_value)) for i in range(1, 20)
+    ]
+    await asyncio.wait(tasks)
     return headline_link_dict
 
 
@@ -234,12 +260,15 @@ def most_popular_monitor():
         'OTHER': 0
     }
     pageid = 0
-    # TODO HARDCODED STOP RANGE
-    for pageid in range(0, 600, 40):
-        url = 'https://forum.overclockers.ua/viewforum.php?f=11&start=%d' % pageid
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
+    while True:
+
+        soup = sync_site_soup(
+            'https://forum.overclockers.ua/viewforum.php?f=11&start=', pageid=pageid)
         headline_tags = soup.findAll('a', {'class': 'topictitle'})
+
+        if soup.find('li', {'class': 'next'}) is None:
+            break
+
         for headline_tag in headline_tags:
             flag = False
             for monitor in monitors_dict:
@@ -250,13 +279,13 @@ def most_popular_monitor():
                     break
             if flag:
                 monitors_dict['OTHER'] += 1
-        pageid += 1
+        pageid += 40
 
     return monitors_dict
 
 
-def five_start_Arthas():
-    soup = soup_site('https://myshows.me/AI_Avenger/wasted')
+def five_stars_Arthas():
+    soup = sync_site_soup('https://myshows.me/AI_Avenger/wasted')
     five_stars_shows = []
     for a in soup.findAll('a'):
         if a.parent.parent.name == 'tr':
@@ -265,21 +294,35 @@ def five_start_Arthas():
     return five_stars_shows
 
 
-if __name__ == "__main__":
-    city = 'Киев'
-    keyword = 'Python'
+def main():
+    #city = 'Киев'
+    #keyword = 'Python'
+
+    # five_stars_Arthas()
+    # five_stars_Arthas()
 
     # dict = dou_jobs(keyword, city)
     # dict = work_ua('python', city='kyiv')
     # dict = rabota_ua('python', city='одесса')
     # dict = djinni('Python', 'Одесса')
 
-    # dict = most_popular_monitor()
-    # dict = habr_python_articles('python')
-    # dict = dota_news('v1lat')
+    #dict = most_popular_monitor()
+    ioloop = asyncio.get_event_loop()
+    #articles = ioloop.run_until_complete(habr_python_articles('python'))
+    value = ioloop.run_until_complete(dota_radiant_winrate())
+    ioloop.close()
+    print(value)
 
-    # for x in dict:
-    #     print(x + "\t" + str(dict[x]))
+    # for x in articles:
+    #     print(x + "\t" + str(articles[x]))
+
+    # dict = dota_news('v1lat')
 
     # print(dota_radiant_winrate())
     # print(five_start_Arthas())
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    main()
+    print("--- %s seconds ---" % (time.time() - start_time))
